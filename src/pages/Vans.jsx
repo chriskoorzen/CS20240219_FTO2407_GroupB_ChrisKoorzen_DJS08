f1ad1c628895c1192eec5bf5554ccf5bef0aea41
Chris Koorzen <fckoorzen@gmail.com>
2024-11-14 16:42:07 +0200

    Add data fetch, and store to State

--- src/pages/Vans.jsx
+++ src/pages/Vans.jsx
@@ -1,6 +1,15 @@
+import { useState, useEffect } from "react"
 
 
 export default function Vans() {
+    const [vehicleData, setVehicleData] = useState(null)
+
+    useEffect(() => {
+        fetch("/api/vans")
+            .then((response) => response.json())
+            .then((json) => setVehicleData(json))
+    }, [])
+
     return (
         <h1>Vans page goes here 🚐</h1>
     )
