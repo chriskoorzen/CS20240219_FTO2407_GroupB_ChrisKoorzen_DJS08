7dc14e8d1187d1f87f5371fcb960212c827fff3b
Chris Koorzen <fckoorzen@gmail.com>
2024-11-14 16:16:00 +0200

    Add new page, refactor nav layout.

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,23 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 
 import Home from "./pages/Home.jsx"
 import About from "./pages/About.jsx"
+import Vans from "./pages/Vans.jsx"
 
 function App() {
 
   return (
     <BrowserRouter basename="DJS/08-ReactRouter/public/">   {/** DEV: Set base project base URL. REMOVE IN PROD */}
-        <nav>
-            <Link to="/">Home</Link>
-            <Link to="/about">About</Link>
-        </nav>
+        <header>
+            <Link className="site-logo" to="/">#VanLife</Link>
+            <nav>
+                <Link to="/about">About</Link>
+                <Link to="/vans">Vans</Link>
+            </nav>
+        </header>
         <Routes>
             <Route path="/" element={<Home />}/>
             <Route path="/about" element={<About />}/>
+            <Route path="/vans" element={<Vans />} />
         </Routes>
     </BrowserRouter>
     
