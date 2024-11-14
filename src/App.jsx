fd5abbe5dd0acf243f23f0345e7268d603ed786b
Chris Koorzen <fckoorzen@gmail.com>
2024-11-14 15:06:44 +0200

    Setup basic App structure and Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,22 @@
-/**
- * Challenge:
- * Bootstrap the VanLife project by creating the first 2 routes:
- * Home and About.
- * 
- * Also include the navbar that can link between the two routes.
- * For now, you'll either need to copy/paste the navbar code
- * to both Home and About pages, or you'll need to find a place
- * to put it where it can be shared between the two pages.
- * (Don't overthink this part - just do whatever is easiest for
- * you because we'll learn a better approach very soon)
- * 
- * Review challenge: do all the CSS yourself based on the design
- * linked in the slides.
- */
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+
+import Home from "./pages/Home.jsx"
+import About from "./pages/About.jsx"
 
 function App() {
 
   return (
-    <h1>Start here</h1>
+    <BrowserRouter basename="DJS/08-ReactRouter/public/">   {/** DEV: Set base project base URL. REMOVE IN PROD */}
+        <nav>
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+        </nav>
+        <Routes>
+            <Route path="/" element={<Home />}/>
+            <Route path="/about" element={<About />}/>
+        </Routes>
+    </BrowserRouter>
+    
   )
 }
 
