1d269463cc38cbef1daa41cd8faf30fb98ffb4de
Chris Koorzen <fckoorzen@gmail.com>
2024-11-15 09:33:51 +0200

    Add HostLayout, update routes to relative paths

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import About from "./pages/About.jsx"
 import Vans from "./pages/Vans.jsx"
 import Details from "./pages/Details.jsx"
 import Layout from "./components/Layout.jsx"
+import HostLayout from "./components/HostLayout.jsx"
 import Dashboard from "./pages/host/Dashboard.jsx"
 import Income from "./pages/host/Income.jsx"
 import Reviews from "./pages/host/Reviews.jsx"
@@ -14,19 +15,19 @@ function App() {
 
   return (
     <HashRouter basename="/">
-      <Routes>
-
-        <Route element={<Layout />}>
-          <Route path="/" element={<Home />}/>
-          <Route path="/about" element={<About />}/>
-          <Route path="/vans" element={<Vans />} />
-          <Route path="/vans/:id" element={<Details />} />
-          <Route path="/host" element={<Dashboard />}/>
-          <Route path="/host/income" element={<Income />}/>
-          <Route path="/host/reviews" element={<Reviews />}/>
-        </Route>
-
-      </Routes>
+        <Routes>
+            <Route path="/" element={<Layout />}>
+                <Route index element={<Home />}/>
+                <Route path="about" element={<About />}/>
+                <Route path="vans" element={<Vans />} />
+                <Route path="vans/:id" element={<Details />} />
+                <Route path="host" element={<HostLayout />}>
+                    <Route index element={<Dashboard />}/>
+                    <Route path="income" element={<Income />}/>
+                    <Route path="reviews" element={<Reviews />}/>
+                </Route>
+            </Route>
+        </Routes>
     </HashRouter>
   )
 }
