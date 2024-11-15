3e2616a3dd37038760bea02d86ecb1fd486b6a5a
Chris Koorzen <fckoorzen@gmail.com>
2024-11-15 12:56:29 +0200

    Wire up routes to detailed HostInfo

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
 import { HashRouter, Routes, Route, Link } from "react-router-dom"
 
+import Layout from "./components/Layout.jsx"
 import Home from "./pages/Home.jsx"
 import About from "./pages/About.jsx"
 import Vans from "./pages/Vans.jsx"
 import Details from "./pages/Details.jsx"
-import Layout from "./components/Layout.jsx"
+
 import HostLayout from "./components/HostLayout.jsx"
 import Dashboard from "./pages/host/Dashboard.jsx"
 import Income from "./pages/host/Income.jsx"
 import Reviews from "./pages/host/Reviews.jsx"
 import HostVans from "./pages/host/HostVans.jsx"
 import HostVanDetail from "./pages/host/HostVanDetail.jsx"
+import HostVanPhotos from "./pages/host/HostVanPhotos.jsx"
+import HostVanInfo from "./pages/host/HostVanInfo.jsx"
+import HostVanPricing from "./pages/host/HostVanPricing.jsx"
 
 
 function App() {
@@ -28,7 +32,11 @@ function App() {
                     <Route path="income" element={<Income />}/>
                     <Route path="reviews" element={<Reviews />}/>
                     <Route path="vans" element={<HostVans />}/>
-                    <Route path="vans/:id" element={<HostVanDetail />}/>
+                    <Route path="vans/:id" element={<HostVanDetail />}>
+                        <Route index element={<HostVanInfo />} />
+                        <Route path="pricing" element={<HostVanPricing />} />
+                        <Route path="photos" element={<HostVanPhotos />} />
+                    </Route>
                 </Route>
             </Route>
         </Routes>
