0e2ac766c5f3feda44bab4598900695a3dc9c8a3
Chris Koorzen <fckoorzen@gmail.com>
2024-11-15 08:20:38 +0200

    Update routing with new Layout

--- src/App.jsx
+++ src/App.jsx
@@ -4,27 +4,24 @@ import Home from "./pages/Home.jsx"
 import About from "./pages/About.jsx"
 import Vans from "./pages/Vans.jsx"
 import Details from "./pages/Details.jsx"
+import Layout from "./components/Layout.jsx"
 
 
 function App() {
 
   return (
     <BrowserRouter basename="DJS/08-ReactRouter/public/">   {/** DEV: Set base project base URL. REMOVE IN PROD */}
-        <header>
-            <Link className="site-logo" to="/">#VanLife</Link>
-            <nav>
-                <Link to="/about">About</Link>
-                <Link to="/vans">Vans</Link>
-            </nav>
-        </header>
-        <Routes>
-            <Route path="/" element={<Home />}/>
-            <Route path="/about" element={<About />}/>
-            <Route path="/vans" element={<Vans />} />
-            <Route path="/vans/:id" element={<Details />} />
-        </Routes>
+      <Routes>
+
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />}/>
+          <Route path="/about" element={<About />}/>
+          <Route path="/vans" element={<Vans />} />
+          <Route path="/vans/:id" element={<Details />} />
+        </Route>
+
+      </Routes>
     </BrowserRouter>
-    
   )
 }
 
