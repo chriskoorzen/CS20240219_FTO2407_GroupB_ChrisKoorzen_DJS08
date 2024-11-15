9017fdf7c0a589d3e86a9c8c6c42cd3d9b23f363
Chris Koorzen <fckoorzen@gmail.com>
2024-11-15 09:46:23 +0200

    Change Links to NavLinks, add active styling.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,22 @@
-import { Link } from "react-router-dom"
+import { Link, NavLink } from "react-router-dom"
 
 
 export default function Header(){
+    const activeStyles = {
+        fontWeight: "bold",
+        textDecoration: "underline",
+        color: "#161616"
+    }
+    
+    const setActive = ({isActive}) => {return isActive ? activeStyles : null}
+
     return (
         <header>
             <Link className="site-logo" to="/">#VanLife</Link>
             <nav>
-                <Link to="/host">Host</Link>
-                <Link to="/about">About</Link>
-                <Link to="/vans">Vans</Link>
+                <NavLink to="/host" style={setActive}>Host</NavLink>
+                <NavLink to="/about" style={setActive}>About</NavLink>
+                <NavLink to="/vans" style={setActive}>Vans</NavLink>
             </nav>
         </header>
     )
--- src/components/HostLayout.jsx
+++ src/components/HostLayout.jsx
@@ -1,14 +1,22 @@
-import { Link, Outlet } from "react-router-dom"
+import { NavLink, Outlet } from "react-router-dom"
 
 
 export default function HostLayout(){
+    const activeStyles = {
+        fontWeight: "bold",
+        textDecoration: "underline",
+        color: "#161616"
+    }
+    
+    const setActive = ({isActive}) => {return isActive ? activeStyles : null}
+
     return(
         <>
             <nav className="host-nav">
-                <Link to="/host">Dashboard</Link>
-                <Link to="/host/income">Income</Link>
-                <Link to="/host/vans">Vans</Link>
-                <Link to="/host/reviews">Reviews</Link>
+                <NavLink to="/host" end style={setActive}>Dashboard</NavLink>
+                <NavLink to="/host/income" style={setActive}>Income</NavLink>
+                <NavLink to="/host/vans" style={setActive}>Vans</NavLink>
+                <NavLink to="/host/reviews" style={setActive}>Reviews</NavLink>
             </nav>
             <Outlet />
         </>
