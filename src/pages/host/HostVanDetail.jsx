40df7b2f2a45f492b0d0f5ab185f90841c920d5e
Chris Koorzen <fckoorzen@gmail.com>
2024-11-15 13:02:52 +0200

    Create nav and connect details pages

--- src/pages/host/HostVanDetail.jsx
+++ src/pages/host/HostVanDetail.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react"
-import { useParams, Link, Outlet } from "react-router-dom"
+import { useParams, Link, Outlet, NavLink } from "react-router-dom"
 
 
 export default function HostVanDetail() {
@@ -15,6 +15,14 @@ export default function HostVanDetail() {
     if (!currentVan) {
         return <h1>Loading...</h1>
     }
+
+    const activeStyles = {
+        fontWeight: "bold",
+        textDecoration: "underline",
+        color: "#161616"
+    }
+    const setActive = ({isActive}) => {return isActive ? activeStyles : null}
+
     return (
         <section>
             <Link
@@ -36,6 +44,12 @@ export default function HostVanDetail() {
                         <h4>${currentVan.price}/day</h4>
                     </div>
                 </div>
+
+                <nav className="host-van-detail-nav">
+                    <NavLink to="." end style={setActive}>Details</NavLink>
+                    <NavLink to="pricing" style={setActive}>Pricing</NavLink>
+                    <NavLink to="photos" style={setActive}>Photos</NavLink>
+                </nav>
 
                 <Outlet />
 
