6183a95ffe6079e9c1145132d561845e4158d290
Chris Koorzen <fckoorzen@gmail.com>
2024-11-15 08:25:30 +0200

    Set up Host routes

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ import About from "./pages/About.jsx"
 import Vans from "./pages/Vans.jsx"
 import Details from "./pages/Details.jsx"
 import Layout from "./components/Layout.jsx"
+import Dashboard from "./pages/host/Dashboard.jsx"
+import Income from "./pages/host/Income.jsx"
+import Reviews from "./pages/host/Reviews.jsx"
 
 
 function App() {
@@ -18,6 +21,9 @@ function App() {
           <Route path="/about" element={<About />}/>
           <Route path="/vans" element={<Vans />} />
           <Route path="/vans/:id" element={<Details />} />
+          <Route path="/host" element={<Dashboard />}/>
+          <Route path="/host/income" element={<Income />}/>
+          <Route path="/host/reviews" element={<Reviews />}/>
         </Route>
 
       </Routes>
